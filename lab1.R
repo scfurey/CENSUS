53270935c6e3caf94b6a20868815d215b1c78203
Spencer Furey <SFurey@spencers-mbp-3.wireless.dartmouth.edu>
2016-09-21 13:45:59 -0400

    initial commit.

---
+++ lab1.R
@@ -0,0 +1,10 @@
+library(dplyr)
+title <- c("Moby Dick", "Huck Finn", "Scarlet Letter")
+author <- c("Herman Melville", "Mark Twain", "Nathaniel Hawthorne")
+published <- c(1851, 1884, 1846) 
+fake <- c("value 1", "value 2")
+
+df <- data.frame(title, author, published)
+
+
+%>%
